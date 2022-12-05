6713cf3382841f6e947737cc97e55f2c54b8efc0
ijsKoud <github@ijskoud.dev>
2022-12-05 20:41:03 +0100

    feat: add readyEvent

---
+++ src/bot/events/ReadyEvent.ts
@@ -0,0 +1,12 @@
+import { ApplyOptions, EventListener, EventListenerOptions } from "@snowcrystals/iglo";
+import { bold } from "colorette";
+
+@ApplyOptions<EventListenerOptions>({
+	name: "ready",
+	once: true
+})
+export default class extends EventListener {
+	public override run() {
+		this.client.logger.info(`${bold(this.client.user!.tag)} is uo and running.`);
+	}
+}
