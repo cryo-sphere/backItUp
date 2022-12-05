076411cdb7c4503d8c4728be64be3801905575ca
ijsKoud <github@ijskoud.dev>
2022-12-05 20:54:35 +0100

    chore: add LogLevel based on NODE_ENV

--- src/lib/Client.ts
+++ src/lib/Client.ts
@@ -1,4 +1,4 @@
-import { IgloClient } from "@snowcrystals/iglo";
+import { IgloClient, LogLevel } from "@snowcrystals/iglo";
 import { GatewayIntentBits } from "discord.js";
 import { join } from "node:path";
 import { fileURLToPath } from "node:url";
@@ -11,6 +11,7 @@ export default class BackItUpClient extends IgloClient {
 	public constructor() {
 		super({
 			errorHandler,
+			logger: { level: process.env.NODE_ENV === "development" ? LogLevel.Debug : LogLevel.Info },
 			paths: { commands: join(basePath, "commands"), events: join(basePath, "events"), interactions: join(basePath, "interactions") },
 			client: { intents: [GatewayIntentBits.Guilds] }
 		});
