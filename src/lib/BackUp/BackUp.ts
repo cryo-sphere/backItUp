87275614b403f3380f3dbc7e49e4134dacf19c4c
ijsKoud <github@ijskoud.dev>
2022-12-14 17:51:58 +0100

    feat(BackUp): add raw database data parsing

--- src/lib/BackUp/BackUp.ts
+++ src/lib/BackUp/BackUp.ts
@@ -1,11 +1,16 @@
 import type { Guild } from "discord.js";
 import type BackItUpClient from "../Client.js";
+import type { BackUpBan, BackUpData, BackUpEmoji, RawBackUp, BackUpSticker, BackUpRole } from "./type.js";
 
 export default class BackUp {
 	public guild!: Guild;
-	public constructor(public client: BackItUpClient, public data: BackUpData) {}
+	public id: string;
 
-	public load(): boolean {
+	public constructor(public client: BackItUpClient, public data: BackUpData) {
+		this.id = data.guild.id;
+	}
+
+	public loadGuild(): boolean {
 		const guild = this.data.guild.id
 			? this.client.guilds.cache.get(this.data.guild.id)
 			: this.client.guilds.cache.find((g) => g.name === this.data.guild.name);
@@ -15,81 +20,61 @@ export default class BackUp {
 		return true;
 	}
 
-	public static parse(client: BackItUpClient, data: any) {
-		return new BackUp(client, data);
+	public static parse(client: BackItUpClient, data: RawBackUp) {
+		const bans = data.bans.map<BackUpBan>((ban) => ({ member: ban.userId, reason: ban.reason }));
+		const emojis = data.emojis.map<BackUpEmoji>((emoji) => ({ name: emoji.name, image: emoji.emoji }));
+		const stickers = data.stickers.map<BackUpSticker>((sticker) => ({
+			name: sticker.name,
+			emoji: sticker.emoji,
+			description: sticker.description,
+			image: sticker.sticker
+		}));
+		const roles = data.roles.map<BackUpRole>((role) => ({
+			name: role.name,
+			id: role.id ?? undefined,
+			postion: role.position,
+			permissions: role.permissions,
+			color: role.color,
+			display: role.displaySeparate,
+			members: role.members,
+			mention: role.mentionEveryone,
+			icon: role.icon ?? undefined
+		}));
+
+		const backUp: BackUpData = {
+			guild: {
+				id: data.backUpId,
+				name: data.guildName,
+				nitroProgress: data.nitroProgressEnabled,
+				banner: data.guildBanner ?? undefined,
+				icon: data.guildIcon ?? undefined,
+				guildId: data.guildId ?? undefined,
+				inviteBackground: data.inviteBackground ?? undefined,
+				inactive: {
+					channelId: data.inactiveChannelId ?? undefined,
+					channelName: data.inactiveChannelName ?? undefined,
+					timeout: data.inactiveTimeout
+				},
+				system: {
+					channelId: data.systemChannelId ?? undefined,
+					channelName: data.systemChannelName ?? undefined,
+					join: data.systemEnableJoin,
+					boost: data.systemEnableBoost,
+					tips: data.systemEnableTips,
+					stickerButton: data.systemEnableSticker
+				}
+			},
+			widget: {
+				enabled: data.widgetEnabled,
+				channelId: data.widgetChannelId ?? undefined,
+				channelName: data.widgetChannelName ?? undefined
+			},
+			bans,
+			emojis,
+			roles,
+			stickers
+		};
+
+		return new BackUp(client, backUp);
 	}
 }
-
-interface BackUpData {
-	guild: BackUpGuild;
-	widget: BackUpWidget;
-
-	// automod stuff
-	// community stuff
-
-	roles: BackUpRole[];
-	bans: BackUpBan[];
-
-	emojis: BackUpEmoji[];
-	stickers: BackUpSticker[];
-}
-
-interface BackUpGuild {
-	name: string;
-	guildId?: string;
-	id: string; // secret identifier for retrieving data from from other server (only )
-	icon: ImageHash;
-	inactive: {
-		channel?: string;
-		timeout: number;
-	};
-	system: {
-		channel: string | null;
-		join: boolean;
-		stickerButton: boolean;
-		boost: boolean;
-		tips: boolean;
-	};
-	nitroProgress: boolean;
-	inviteBackground?: ImageHash; // todo image type
-	banner?: ImageHash;
-}
-
-interface BackUpWidget {
-	enabled: boolean;
-	channel: string;
-}
-
-interface BackUpRole {
-	name: string;
-	id?: string;
-	postion: number;
-
-	icon?: ImageHash;
-	color: string;
-
-	mention: boolean;
-	display: boolean;
-
-	permissions: bigint;
-	members: string[];
-}
-
-interface BackUpBan {
-	member: string;
-	reason: string;
-}
-
-interface BackUpEmoji {
-	name: string;
-	image: ImageHash;
-}
-
-interface BackUpSticker {
-	name: string;
-	emoji: string;
-	description?: string;
-	image: ImageHash;
-}
-
-type ImageHash = string;
---
+++ src/lib/BackUp/type.ts
@@ -0,0 +1,93 @@
+import type {
+	BackUp as RawBackUpType,
+	BackUpBan as RawBackUpBan,
+	BackUpEmoji as RawBackUpEmoji,
+	BackUpRole as RawBackUpRole,
+	BackUpSticker as RawBackUpSticker
+} from "@prisma/client";
+
+interface RawBackUpComplete {
+	bans: RawBackUpBan[];
+	emojis: RawBackUpEmoji[];
+	roles: RawBackUpRole[];
+	stickers: RawBackUpSticker[];
+}
+
+export type RawBackUp = RawBackUpType & RawBackUpComplete;
+
+export interface BackUpData {
+	guild: BackUpGuild;
+	widget: BackUpWidget;
+
+	// automod stuff
+	// community stuff
+
+	roles: BackUpRole[];
+	bans: BackUpBan[];
+
+	emojis: BackUpEmoji[];
+	stickers: BackUpSticker[];
+}
+
+export interface BackUpGuild {
+	name: string;
+	guildId?: string;
+	id: string; // secret identifier for retrieving data from from other server (only )
+	icon?: ImageHash;
+	inactive: {
+		channelId?: string;
+		channelName?: string;
+		timeout: number;
+	};
+	system: {
+		channelId?: string;
+		channelName?: string;
+		join: boolean;
+		stickerButton: boolean;
+		boost: boolean;
+		tips: boolean;
+	};
+	nitroProgress: boolean;
+	inviteBackground?: ImageHash; // todo image type
+	banner?: ImageHash;
+}
+
+export interface BackUpWidget {
+	enabled: boolean;
+	channelId?: string;
+	channelName?: string;
+}
+
+export interface BackUpRole {
+	name: string;
+	id?: string;
+	postion: number;
+
+	icon?: ImageHash;
+	color: string;
+
+	mention: boolean;
+	display: boolean;
+
+	permissions: bigint;
+	members: string[];
+}
+
+export interface BackUpBan {
+	member: string;
+	reason: string;
+}
+
+export interface BackUpEmoji {
+	name: string;
+	image: ImageHash;
+}
+
+export interface BackUpSticker {
+	name: string;
+	emoji: string;
+	description?: string;
+	image: ImageHash;
+}
+
+export type ImageHash = string;
